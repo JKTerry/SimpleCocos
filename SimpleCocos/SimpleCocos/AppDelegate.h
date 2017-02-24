2b4f66af6f95d092f9272c37226bfaf126a5f748
jinke <jinke@goluk.com>
2017-02-24 17:39:28 +0800

    add new project

---
+++ SimpleCocos/SimpleCocos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCocos
+//
+//  Created by mac on 2017/2/24.
+//  Copyright © 2017年 JK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
